d4c6837acd7c9a1a1a8795da8da71be9795dd704
Patrick <patrick-pcs@hotmail.com>
2022-08-12 21:01:18 -0300

    primeiro commit

---
+++ GeradorDeApostas/Controllers/MegaSenaController.cs
@@ -0,0 +1,49 @@
+﻿using GeradorDeApostas.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace GeradorDeApostas.Controllers
+{
+    [ApiController]
+    [Route(template: "v1")]
+    public class MegaSenaController : ControllerBase
+    {
+
+        [HttpGet]
+        [Route(template: "seis-numeros")]
+        public async Task<IActionResult> SeisNumeros()
+        {
+            var numeros = new MegaSena();
+            numeros.Id = 1;
+
+            Random randNum = new Random();
+            int numerosSorteado;
+            int[] verificador = new int[6]; // um vetor instanciado recebe o numero zero em todas as posições
+
+
+            for (int i = 0; i < 6; i++)
+            {
+
+            Inicio:
+                numerosSorteado = randNum.Next(1, 6+1); //precisa colocar o mais um pois tem um bug que se nao tiver nao pega o ultimo numero
+
+                for (int j = 0; j < 6; j++)
+                {
+                    if (verificador[j] == numerosSorteado)
+                    {
+                        goto Inicio; //refaz os passos acima caso tenha algum numero repetido
+                    }
+                }
+
+                numeros.Numeros.Add(numerosSorteado);
+                verificador[i] = numerosSorteado;
+
+            }
+
+            return Ok(numeros);
+
+        }
+    }
+}
---
+++ GeradorDeApostas/Models/MegaSena.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace GeradorDeApostas.Models
+{
+    public class MegaSena
+    {
+        public int Id { get; set; }
+        public List<int> Numeros { get; set; }
+
+        public MegaSena()
+        {
+            Numeros = new List<int>();
+        }
+    }
+
+    
+}
