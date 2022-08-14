946cc9bd9eaf5bd3a311821fdb32cddf49eba392
Patrick <patrick-pcs@hotmail.com>
2022-08-14 12:22:42 -0300

    acerto na variação de numeros por apostas

--- GeradorDeApostas/Controllers/MegaSenaController.cs
+++ GeradorDeApostas/Controllers/MegaSenaController.cs
@@ -12,24 +12,33 @@ namespace GeradorDeApostas.Controllers
     {
 
         [HttpGet]
-        [Route(template: "seis-numeros")]
-        public async Task<IActionResult> SeisNumeros()
+        [Route(template: "mega-sena/{quantidade}")] //quantidade de numeros em uma aposta (6 a 15)
+        //public async Task<IActionResult> SeisNumeros([FromRoute] int quantidade)
+        public IActionResult SeisNumeros([FromRoute] int quantidade)
         {
+            if (!ModelState.IsValid)
+                return BadRequest();
+
+            if (quantidade < 6 || quantidade > 15)
+            {
+                return BadRequest("A quantidade precisa ser entre 6 e 15");
+            }
+
             var numeros = new MegaSena();
             numeros.Id = 1;
 
             Random randNum = new Random();
             int numerosSorteado;
-            int[] verificador = new int[6]; // um vetor instanciado recebe o numero zero em todas as posições
+            int[] verificador = new int[quantidade]; // um vetor instanciado recebe o numero zero em todas as posições
 
 
-            for (int i = 0; i < 6; i++)
+            for (int i = 0; i < quantidade; i++)
             {
 
             Inicio:
-                numerosSorteado = randNum.Next(1, 6+1); //precisa colocar o mais um pois tem um bug que se nao tiver nao pega o ultimo numero
+                numerosSorteado = randNum.Next(1, quantidade + 1); //precisa colocar o mais um pois tem um bug que se nao tiver nao pega o ultimo numero
 
-                for (int j = 0; j < 6; j++)
+                for (int j = 0; j < quantidade; j++)
                 {
                     if (verificador[j] == numerosSorteado)
                     {
